7bbac05818b6f1b68e0823cc21b2481bc8dbeac2
rrochavictor <vitinhodarocha2@gmail.com>
2026-05-10 12:20:18 +0100

    Atualizando

---
+++ src/YouVideo/AuthorAll.java
@@ -0,0 +1,12 @@
+package YouVideo;
+
+interface AuthorAll extends Author {
+    void addPodcast(Podcast podcast);
+
+    void removePodcast(Podcast podcast);
+
+    void addShow(Show show);
+
+    void removeShow(Show show);
+}
+
